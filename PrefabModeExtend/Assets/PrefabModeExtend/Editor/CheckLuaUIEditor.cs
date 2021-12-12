bff0f016aa6d2cb87dd6c20fca8238a18c842a51
DESKTOP-DM584UI\luxuming <nada_havok@163.com>
2021-12-13 00:12:51 +0800

    1

---
+++ PrefabModeExtend/Assets/PrefabModeExtend/Editor/CheckLuaUIEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace PrefabModeExtend
+{
+    public sealed class CheckLuaUIEditor
+    {
+        [MenuItem("Assets/PrefabMode/RunCheck")]
+        public static void RunCheck()
+        {
+            UIAnnotationCheck checker = new UIAnnotationCheck();
+            string prefabName = "MonopolyHook";
+            bool check = checker.checkPrefabLuaUIComplete(prefabName);
+            Debug.Log(" check " + check);
+        }
+    }
+}
